480611bdd295029b1e4c449e49da4343b8cea8ee
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-20 18:17:35 -0400

    remove old fixtures

--- fixtures/comment.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-
-function read(fp) {
-  return fs.readFileSync(path.resolve('fixtures', fp), 'utf8');
-}
--- fixtures/config-comment.js
+++
@@ -1,9 +0,0 @@
-/* eslint-do-stuff */
-'use strict';
-
-var fs = require('fs');
-var path = require('path');
-
-function read(fp) {
-  return fs.readFileSync(path.resolve('fixtures', fp), 'utf8');
-}
